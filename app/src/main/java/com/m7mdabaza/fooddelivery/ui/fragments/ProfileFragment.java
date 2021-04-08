46ebc11a2e803a80b7ddcfa6b68e1afeda11276e
M7md-Abaza <abaza.jobs@gmail.com>
2021-04-09 00:08:51 +0200

    Delivery Food

--- app/src/main/java/com/m7mdabaza/fooddelivery/ui/fragments/ProfileFragment.java
+++ app/src/main/java/com/m7mdabaza/fooddelivery/ui/fragments/ProfileFragment.java
@@ -3,16 +3,26 @@ package com.m7mdabaza.fooddelivery.ui.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.m7mdabaza.fooddelivery.R;
+import com.m7mdabaza.fooddelivery.ui.activities.RestaurantActivity;
+import com.m7mdabaza.fooddelivery.ui.adapters.ProfileRecyclerAdapter;
+import com.m7mdabaza.fooddelivery.ui.adapters.RestaurantRecyclerAdapter;
+import com.m7mdabaza.fooddelivery.ui.models.ProfileModel;
+import com.m7mdabaza.fooddelivery.ui.models.RestaurantModel;
+
+import java.util.ArrayList;
 
 
 public class ProfileFragment extends Fragment {
 
+    RecyclerView profileInfoRecycler;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -20,9 +30,31 @@ public class ProfileFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        profileInfoRecycler = view.findViewById(R.id.profileInfoRecycler);
+
+        profileInfoRecycler.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false));
+        ProfileRecyclerAdapter profileRecyclerAdapter = new ProfileRecyclerAdapter();
+        profileRecyclerAdapter.setList(getProfileInfoList());
+        profileInfoRecycler.setAdapter(profileRecyclerAdapter);
+
+        return view;
+    }
+
+
+    private ArrayList getProfileInfoList() {
+        ArrayList<ProfileModel> profileInfoList = new ArrayList<>();
+
+        profileInfoList.add(new ProfileModel("My orders"));
+        profileInfoList.add(new ProfileModel("My payment cards"));
+        profileInfoList.add(new ProfileModel("My addresses"));
+        profileInfoList.add(new ProfileModel("Promocodes"));
+        profileInfoList.add(new ProfileModel("Share promocodes"));
+        profileInfoList.add(new ProfileModel("FAQ"));
+        profileInfoList.add(new ProfileModel("Exit"));
+
+        return profileInfoList;
     }
 }
---
+++ app/src/main/java/com/m7mdabaza/fooddelivery/ui/models/ProfileModel.java
@@ -0,0 +1,17 @@
+package com.m7mdabaza.fooddelivery.ui.models;
+
+public class ProfileModel {
+    String profileInfo;
+
+    public ProfileModel(String profileInfo) {
+        this.profileInfo = profileInfo;
+    }
+
+    public String getProfileInfo() {
+        return profileInfo;
+    }
+
+    public void setProfileInfo(String profileInfo) {
+        this.profileInfo = profileInfo;
+    }
+}
